02237df15042461c1d052950902cec0b4ac7d261
Guilherme <guigomes.ti@gmail.com>
2023-11-13 16:03:49 -0400

    Primeiro commit

---
+++ src/main/java/br/com/astra/developer/dto/TesteResponse.java
@@ -0,0 +1,16 @@
+package br.com.astra.developer.dto;
+
+public class TesteResponse {
+
+	public String data;
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	} 
+	
+	
+}
---
+++ src/test/java/com/astra/template/AstraTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.astra.developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstraTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
